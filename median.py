0debb0b6760dbf7cdf74c5179d91e863c96884c9
Jason Cai <jiaxin.cai@kcl.ac.uk>
2023-10-04 19:47:54 +0100

    Add median calculator

--- median.py
+++ median.py
@@ -1,6 +1,20 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+def chooseMedian(numbers):
+    numbers.sort()
+    
+    n = len(numbers)
+    
+    if n%2 == 1:
+        median = numbers[n//2]
+        
+    elif n%2 == 0:
+        middle1 = numbers[n//2 -1]
+        middle2 = numbers[n//2 ]
+        median = (middle1 + middle2)/2
+        
+    return median
+    
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +24,4 @@ while True:
     else:
         break
 print(numbers)
+print(chooseMedian(numbers))
